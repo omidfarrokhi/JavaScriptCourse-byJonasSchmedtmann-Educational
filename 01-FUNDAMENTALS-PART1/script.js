56e95bf9d248360566cdcd041fa510eb024b0b2c
Omid <omidfarrokhii1381@gmail.com>
2026-09-13 21:40:39 +0330

    equality operators (==) and (===)

--- 01-FUNDAMENTALS-PART1/script.js
+++ 01-FUNDAMENTALS-PART1/script.js
@@ -1,21 +1,26 @@
-// 5 falsy value : 0, "", undefined, null, NaN
+const age = 18;
 
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("Jonas"));
-console.log(Boolean({}));
-console.log(Boolean(""));
-
-const money = 0;
-if (money) {
-  console.log("Don't spend it all :)");
-} else {
-  console.log("You should get a job!");
+if (age === 18) {
+  console.log("You just became an adult");
 }
 
-let height;
-if (height) {
-  console.log("YAY! Height is defined");
+// loose equality operator
+console.log("18" == 18);
+// strict equality operator
+console.log("18" === "18");
+
+const fav = prompt("What's your favourite number?");
+console.log(fav);
+console.log(typeof fav);
+
+if (fav === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (fav === 7) {
+  console.log("7 is also a cool number");
 } else {
-  console.log("Height is UNDEFINED");
+  console.log("number is not 23 or 7");
+}
+
+if (fav !== 23) {
+  console.log("why not 23?");
 }
