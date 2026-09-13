da253baf601c4e08ee53ebbaa733f8b782345923
Omid <omidfarrokhii1381@gmail.com>
2026-09-14 02:22:04 +0330

    functions

--- 02-Fundamentals-Part2/script.js
+++ 02-Fundamentals-Part2/script.js
@@ -1,11 +1,23 @@
 "use strict";
 
-let hasDriversLicense = false;
-const passTest = true;
+// defining a function
+function logger() {
+  console.log("my name is Jonas");
+}
 
-if (passTest) {
-  hasDriverLiscense = true;
+// calling, running, invoking a function
+logger();
+logger();
+logger();
+
+// function can also return
+function fruitProcessor(apples, oranges) {
+  const juice = `juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
 }
-if (hasDriversLicense) {
-  console.log("I can drive :D");
-}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
