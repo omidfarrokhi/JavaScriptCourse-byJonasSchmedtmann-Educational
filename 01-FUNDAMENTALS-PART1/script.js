f8c439141f4baaa57b1a95a581d7a2824b9609cb
Omid <omidfarrokhii1381@gmail.com>
2026-09-13 22:06:08 +0330

    the switch statement

--- 01-FUNDAMENTALS-PART1/script.js
+++ 01-FUNDAMENTALS-PART1/script.js
@@ -1,17 +1,41 @@
-const hasDriverLicense = true;
-const hasGoodVision = true;
+const day = "Monday";
 
-console.log(hasDriverLicense && hasGoodVision);
-console.log(hasDriverLicense || hasGoodVision);
-console.log(!hasDriverLicense);
-
-const canDrive = hasDriverLicense && hasGoodVision;
-
-if (canDrive) {
-  console.log("Sarah is able to drive");
-} else {
-  console.log("Someone else should drive");
+switch (day) {
+  case "Monday": // day === "Monday"
+    console.log("plan course structure");
+    console.log("go to coding meetup");
+    break;
+  case "Tuesday":
+    console.log("prepare theory videos");
+    break;
+  case "Wednesday":
+  case "Thursday":
+    console.log("write code examples");
+    break;
+  case "Friday":
+    console.log("record videos");
+    break;
+  case "Saturday":
+  case "Sunday":
+    console.log("enjoy the weekend :D");
+    break;
+  default:
+    console.log("not a valid day!");
+    break;
 }
 
-const isTired = true;
-console.log(hasDriverLicense && hasGoodVision && isTired);
+// the same logic with if & else
+if (day === "Monday") {
+  console.log("plan course structure");
+  console.log("go to coding meetup");
+} else if (day === "Tuesday") {
+  console.log("prepare theory videos");
+} else if (day === "Wednesday" || day === "Thursday") {
+  console.log("write code examples");
+} else if (day === "Friday") {
+  console.log("record videos");
+} else if (day === "Saturday" || day === "Sunday") {
+  console.log("enjoy the weekend :D");
+} else {
+  console.log("not a valid day!");
+}
