3cbe1fdd25104bff5d9ffe628a3dac3966a779cd
Omid <omidfarrokhii1381@gmail.com>
2026-09-13 22:36:51 +0330

    statements & expressions

--- 01-FUNDAMENTALS-PART1/script.js
+++ 01-FUNDAMENTALS-PART1/script.js
@@ -1,41 +1,13 @@
-const day = "Monday";
+// expressions
+3 + 4;
+1991;
+true && falso && !false;
 
-switch (day) {
-  case "Monday": // day === "Monday"
-    console.log("plan course structure");
-    console.log("go to coding meetup");
-    break;
-  case "Tuesday":
-    console.log("prepare theory videos");
-    break;
-  case "Wednesday":
-  case "Thursday":
-    console.log("write code examples");
-    break;
-  case "Friday":
-    console.log("record videos");
-    break;
-  case "Saturday":
-  case "Sunday":
-    console.log("enjoy the weekend :D");
-    break;
-  default:
-    console.log("not a valid day!");
-    break;
+// statements
+if (23 > 10) {
+  const str = "23 is bigger";
 }
 
-// the same logic with if & else
-if (day === "Monday") {
-  console.log("plan course structure");
-  console.log("go to coding meetup");
-} else if (day === "Tuesday") {
-  console.log("prepare theory videos");
-} else if (day === "Wednesday" || day === "Thursday") {
-  console.log("write code examples");
-} else if (day === "Friday") {
-  console.log("record videos");
-} else if (day === "Saturday" || day === "Sunday") {
-  console.log("enjoy the weekend :D");
-} else {
-  console.log("not a valid day!");
-}
+// accepts expressions
+// you can't write a statement inside a template literal
+console.log(`I'm ${2037 - 1991} years old`);
