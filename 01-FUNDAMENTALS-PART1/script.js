614cffd21ab1bdd856bf647b4abf419008dbe3e5
Omid <omidfarrokhii1381@gmail.com>
2026-09-13 15:30:42 +0330

    values & variables

--- 01-FUNDAMENTALS-PART1/script.js
+++ 01-FUNDAMENTALS-PART1/script.js
@@ -1,4 +1,28 @@
 let js = "amazing";
-if (js == "amazing") alert("JS is fun!!!");
 
 console.log(40 + 8 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Vars shouldnt start with numbers
+// don't start with special characters
+// don't use keywords
+
+// conventions
+// don't start with upper case (it's for objects)
+// always be declerative
+
+// don't
+let job1 = "programmer";
+let job2 = "athlete";
+
+// do
+let myFirstJob = "Programmer";
+let mySecondJob = "athlete";
