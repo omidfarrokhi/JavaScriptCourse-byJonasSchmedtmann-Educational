63677a7907931314badd57fcf8f9c381c6d8fcb5
Omid <omidfarrokhii1381@gmail.com>
2026-09-13 23:35:01 +0330

    activating strict mode

---
+++ 02-Fundamentals-Part2/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLiscense = true;
+}
+if (hasDriversLicense) {
+  console.log("I can drive :D");
+}
