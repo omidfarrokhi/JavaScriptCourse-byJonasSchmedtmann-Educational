35f6290ef5d2ed00a1ba5b26853d7bfca438a9f2
Omid <omidfarrokhii1381@gmail.com>
2026-09-14 18:12:32 +0330

    looping arrays, breaking & continuing

--- 02-Fundamentals-Part2/script.js
+++ 02-Fundamentals-Part2/script.js
@@ -1,6 +1,40 @@
 "use strict";
 
-// for loop in JS
-for (let rep = 1; rep <= 10; rep++) {
-  console.log(`lifting weights repetition ${rep}🏋️`);
+const jonas = [
+  "jonas",
+  "schmedtmann",
+  46,
+  "teacher",
+  ["micheal", "peter", "steven"],
+  true,
+];
+const types = [];
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2026 - years[i]);
+}
+
+console.log(ages);
+
+// continue and break
+
+// only strings
+console.log("---only strings-----");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+// break with numbers
+console.log("---break with number----");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") {
+    break;
+  }
+
+  console.log(jonas[i], typeof jonas[i]);
 }
