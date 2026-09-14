8593c2cf6db90163bf105d7aab802e7538db8017
Omid <omidfarrokhii1381@gmail.com>
2026-09-14 14:43:29 +0330

    functions calling other functions

--- 02-Fundamentals-Part2/script.js
+++ 02-Fundamentals-Part2/script.js
@@ -1,29 +1,15 @@
 "use strict";
 
-// function declaration
-function calcAge(birthYear) {
-  return 2037 - birthYear;
+function cutFruits(fruit) {
+  return fruit * 4;
 }
 
-const age1 = calcAge(1991);
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruits(apples);
+  const orangePieces = cutFruits(oranges);
 
-// function expression
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
-};
+  const juice = `juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+  return juice;
+}
 
-const age2 = calcAge2(1991);
-
-// arrow function
-const calcAge3 = (birthYear) => 2037 - birthYear;
-
-const age3 = calcAge3(1991);
-
-const yearsUntilRetirement = (birthYear, firstName) => {
-  const age = 2037 - birthYear;
-  const retirement = 65 - age;
-  return `${firstName} retires in ${retirement} years`;
-};
-
-console.log(yearsUntilRetirement(1991, "jonas"));
-console.log(yearsUntilRetirement(1980, "Bob"));
+console.log(fruitProcessor(2, 3));
