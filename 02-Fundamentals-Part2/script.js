9b1b73479416d3985283521fd31e378d2a8cd0ae
Omid <omidfarrokhii1381@gmail.com>
2026-09-14 17:43:03 +0330

    object methods

--- 02-Fundamentals-Part2/script.js
+++ 02-Fundamentals-Part2/script.js
@@ -4,30 +4,32 @@
 const jonas = {
   firstName: "jonas",
   lastName: "schmedtmann",
-  age: 2037 - 1991,
+  birthYear: 1991,
   job: "teacher",
   friends: ["micheal", "peter", "steven"],
+  hasDriversLicense: true,
+
+  // functions are just a value
+  // just like bool, number, array values
+
+  ////// not so good method
+
+  /*
+  calcAge: function (birthYear) {
+    return 2026 - birthYear;
+  },
+  */
+
+  ////// better method (don't repeat ys)
+
+  calcAge: function () {
+    return 2026 - this.birthYear;
+  },
 };
 
-console.log(jonas);
-console.log(jonas.lastName);
-console.log(jonas["lastName"]);
+/*
+console.log(jonas.calcAge(1991));
+console.log(jonas["calcAge"](1991));
+*/
 
-// you can use expression on bracket but not on dot
-const nameKey = "Name";
-console.log(jonas["first" + nameKey]);
-
-const interestedIn = prompt("what do you want to know?");
-// error: the output is undefined
-// jonas has no property named interestedIn :?
-console.log(jonas.interestedIn);
-console.log(jonas[interestedIn]);
-
-// modifying object
-jonas.location = "portugal";
-jonas["twitter"] = "@jonasschmedtman";
-console.log(jonas);
-
-console.log(
-  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`,
-);
+console.log(jonas.calcAge());
