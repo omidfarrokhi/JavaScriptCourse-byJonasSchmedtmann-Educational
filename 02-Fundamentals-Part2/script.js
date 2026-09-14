ea4735108d4ad41feb86fd3df74a4c8c059c42d2
Omid <omidfarrokhii1381@gmail.com>
2026-09-14 16:51:53 +0330

    dot & bracket notation

--- 02-Fundamentals-Part2/script.js
+++ 02-Fundamentals-Part2/script.js
@@ -8,3 +8,26 @@ const jonas = {
   job: "teacher",
   friends: ["micheal", "peter", "steven"],
 };
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+// you can use expression on bracket but not on dot
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+
+const interestedIn = prompt("what do you want to know?");
+// error: the output is undefined
+// jonas has no property named interestedIn :?
+console.log(jonas.interestedIn);
+console.log(jonas[interestedIn]);
+
+// modifying object
+jonas.location = "portugal";
+jonas["twitter"] = "@jonasschmedtman";
+console.log(jonas);
+
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`,
+);
