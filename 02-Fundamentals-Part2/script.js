b309e456da440a76ccca65253bf180df3bcb5a62
Omid <omidfarrokhii1381@gmail.com>
2026-09-14 17:55:09 +0330

    the for loop

--- 02-Fundamentals-Part2/script.js
+++ 02-Fundamentals-Part2/script.js
@@ -1,35 +1,6 @@
 "use strict";
 
-// JS object's literal notation
-const jonas = {
-  firstName: "jonas",
-  lastName: "schmedtmann",
-  birthYear: 1991,
-  job: "teacher",
-  friends: ["micheal", "peter", "steven"],
-  hasDriversLicense: true,
-
-  // functions are just a value
-  // just like bool, number, array values
-
-  ////// not so good method
-
-  /*
-  calcAge: function (birthYear) {
-    return 2026 - birthYear;
-  },
-  */
-
-  ////// better method (don't repeat ys)
-
-  calcAge: function () {
-    return 2026 - this.birthYear;
-  },
-};
-
-/*
-console.log(jonas.calcAge(1991));
-console.log(jonas["calcAge"](1991));
-*/
-
-console.log(jonas.calcAge());
+// for loop in JS
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`lifting weights repetition ${rep}🏋️`);
+}
