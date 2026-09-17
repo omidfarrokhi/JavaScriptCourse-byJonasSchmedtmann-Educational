86c955639b2639bd3991e56b8af5b3804b66f24f
Omid <omidfarrokhii1381@gmail.com>
2026-09-17 16:36:36 +0330

    manipulating CSS styles

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -9,10 +9,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '🚫No number!';
+
+    // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈Too High!';
@@ -22,6 +31,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉Too Low!';
