106f0d15202fe438c493c42cadda42b36baee2f5
Omid <omidfarrokhii1381@gmail.com>
2026-09-17 17:36:58 +0330

    implementing highscore

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -21,6 +21,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // when guess is too high
   } else if (guess > secretNumber) {
