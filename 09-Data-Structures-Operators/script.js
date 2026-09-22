f1b2413e779a890f9f42fc77de20040f694c7977
Omid <omidfarrokhii1381@gmail.com>
2026-09-22 14:57:26 +0330

    looping arrays with for-of loop

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,72 +1,59 @@
 'use strict';
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavainti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzaria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, //open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // using object destructuring for function arguments
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `order recieved!!! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+
+  // use the spread operator on function parameters
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
   },
 };
 
-// 1.
-const [players1, players2] = game.players;
-console.log(players1, players2);
-// 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(gk, fieldPlayers);
-// 3.
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers);
-// 4.
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-// 5.
-const { team1, x: draw, team2 } = game.odds;
-// or also you can
-// const {odds: {team1, x: draw, team2}} = game;
-console.log(team1, draw, team2);
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-// 6.
-const printGoals = function (...players) {
-  console.log(`${players.length} goals were scored`);
-};
+for (const item of menu) {
+  console.log(item);
+}
 
-// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-// printGoals('Davies', 'Muller');
-printGoals(...game.scored);
-
-// 7.
-team1 < team2 && console.log('Team1 is more likely to win');
-team1 > team2 && console.log('Team2 is more likely to win');
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
