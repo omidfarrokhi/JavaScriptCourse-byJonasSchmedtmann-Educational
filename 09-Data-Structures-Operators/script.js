fddfec0d2f1ffd7f1d4466101e1447d6e5c1c69e
Omid <omidfarrokhii1381@gmail.com>
2026-09-22 16:47:17 +0330

    looping objects Object keys, values, and entries

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -56,33 +56,23 @@ const restaurant = {
   },
 };
 
-//// reading the value of undefined
-// if (restaurant.openingHours.mon) {
-// console.log(restaurant.openingHours.mon.open);
-// }
+// property names
+const properties = Object.keys(openingHours);
+console.log(properties);
 
-//// actually it's far worst ;??
-// if (restaurant.openingHours && restaurant.openingHours.mon) {
-//   console.log(restaurant.openingHours.mon.open);
-// }
-
-// OPTIONAL CHANING
-//// we get undefined not error
-console.log(restaurant.openingHours.mon?.open);
-console.log(restaurant.openingHours?.mon?.open);
-
-const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-
-for (const day of days) {
-  const open = restaurant.openingHours[day]?.open ?? 'closed';
-  console.log(`on ${day} we open at ${open}`);
+let openStr = `We are open on ${properties.length} days:`;
+for (const day of properties) {
+  openStr += `${day},`;
 }
+console.log(openStr);
 
-// on Methods
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+// property values
+const values = Object.values(openingHours);
+console.log(values);
 
-// on Arrays
-const users = [{ name: 'Jonas', email: 'hello@jonas' }];
+// entire object
+const entries = Object.entries(openingHours);
 
-console.log(users[0]?.name ?? 'user array empty');
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we ope at ${open} and close at ${close}.`);
+}
