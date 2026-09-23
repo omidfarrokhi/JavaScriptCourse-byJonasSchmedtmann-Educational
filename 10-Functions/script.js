1696720863e008842869aa5399bd9ae0e017f20f
Omid <omidfarrokhii1381@gmail.com>
2026-09-24 01:09:01 +0330

    default parameters

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const bookings = [];
+
+// adding defaultssssssssssss
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // before ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
