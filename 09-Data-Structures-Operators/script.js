eabd554cdfc7023f6a7888223f64de78c4fdbe1c
Omid <omidfarrokhii1381@gmail.com>
2026-09-23 23:47:41 +0330

    challenge #4

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,53 +1,23 @@
 'use strict';
 
-// Split & Join
-console.log('a+very+nice+string'.split('+'));
-console.log('Jonas Schmedtmann'.split(''));
+// underscore_case
+//  first_name
+//  Some_Variable
+//   claculate_AGE
+//   delayed_departure
 
-const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+const text = document.querySelector('textarea').value;
 
-const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
-console.log(newName);
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
 
-const capitalizeName = function (name) {
-  const namesUpper = [];
-  const names = name.split(' ');
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
 
-  for (const n of names) {
-    namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    // another way
-    // namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20, ' ')}${'✅'.repeat(i + 1)}`);
   }
-  console.log(namesUpper.join(' '));
-};
-
-capitalizeName('jessica and smith davis');
-capitalizeName('jonas schmedtmann');
-
-// Padding
-const message = 'Go to gate 23!';
-console.log(message.padStart(25, '+'));
-console.log(message.padEnd(40, '+'));
-
-// another example
-const maskCreditCard = function (number) {
-  // another way instead of String() method
-  const str = number + '';
-  const last = str.slice(-4);
-  return last.padStart(str.length, '*');
-};
-
-console.log(maskCreditCard(12341253412354));
-console.log(maskCreditCard(2142134));
-console.log(maskCreditCard(51346146167135414513425));
-
-// Repeat
-const message2 = 'Bad weather... All departurs delayed...';
-console.log(message2.repeat(5));
-
-const planesInLine = function (n) {
-  console.log(`There are ${n} planes in line ${'🛬'.repeat(n)}`);
-};
-
-planesInLine(5);
-planesInLine(11);
+});
