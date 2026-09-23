01ee9d9ced0792b3fc262c645d5262a774b891ec
Omid <omidfarrokhii1381@gmail.com>
2026-09-23 17:32:12 +0330

    challenge #3

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,110 +1,35 @@
 'use strict';
 
-const italianFoods = new Set([
-  'pasta',
-  'gnocchi',
-  'tomatoes',
-  'olive oil',
-  'garlic',
-  'basil',
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
 ]);
 
-const mexicanFoods = new Set([
-  'tortillas',
-  'beans',
-  'rice',
-  'tomatoes',
-  'avocado',
-  'garlic',
-]);
+// 1.
+const events = gameEvents.values();
+const eventsNoDuplicate = [...new Set(events)];
+console.log(eventsNoDuplicate);
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// 2.
+gameEvents.delete(64);
 
-const openingHours = {
-  // post ES6 we can also compute properties names
-  // in addition to the values
-  thu: {
-    open: 12,
-    close: 22,
-  },
-  fri: {
-    open: 11,
-    close: 23,
-  },
-  sat: {
-    open: 0, //open 24 hours
-    close: 24,
-  },
-};
+// 3.
+const numEvents = gameEvents.size;
+const avgEvents = 90 / numEvents;
+console.log(avgEvents);
 
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavainti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzaria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  // before ES6
-  // openingHours: openingHours,
-
-  // ES6 enhanced object literal
-  openingHours,
-
-  // ES6 easier way to write methods
-  // no need to explicitly declare function keyword
-  order(starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  // using object destructuring for function arguments
-  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
-    console.log(
-      `order recieved!!! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
-    );
-  },
-
-  // use the spread operator on function parameters
-  orderPasta(ing1, ing2, ing3) {
-    console.log(`here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
-  },
-
-  orderPizza(mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
-
-// better way than .set()
-
-const question = new Map([
-  ['question', 'what is the best programming language?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JS'],
-  ['correct', 3],
-  [true, 'Correct!!!!'],
-  [false, 'try again:('],
-]);
-console.log(question);
-
-// it's similar to Object.entries method!!!!!!
-console.log(Object.entries(openingHours));
-
-// conver object to map
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
-
-// iteration (the quiz app)
-console.log(question.get('question'));
-
-for (const [key, value] of question) {
-  if (typeof key === 'number') {
-    console.log(`Answer ${key}: ${value}`);
-  }
+// 4.
+for (const [time, event] of gameEvents) {
+  console.log(
+    `[${time < 45 ? 'FIRST HALF' : 'SECOND HALF'}] ${time}: ${event}`,
+  );
 }
-
-const answer = Number(prompt('Your answer'));
-console.log(question.get(answer === question.get('correct')));
-
-// convert back map to array
-console.log(...question);
