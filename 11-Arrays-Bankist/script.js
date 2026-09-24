2ffc6e6c2b78f1d54792b0b00aca90fc01fc6c6c
Omid <omidfarrokhii1381@gmail.com>
2026-09-24 23:35:58 +0330

    simple array methods

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -74,3 +74,40 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+
+// shallow coppy
+console.log(arr.slice());
+// or
+console.log([...arr]);
+
+// SPLICE (does mutate the original array)
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+// start at 1 delete 2 (second param is not the end!!!!!!!!!)
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['i', 'j', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+// or
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join('-'));
