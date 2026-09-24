fd9a1588685f4999b39ecee9a476cbe7fccbb5e5
Omid <omidfarrokhii1381@gmail.com>
2026-09-24 16:12:32 +0330

    the call and apply methods

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,17 +1,54 @@
 'use strict';
 
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
 };
 
-const greeterHey = greet('Hey');
-greeterHey('Jonas');
-greeterHey('Omid');
+lufthansa.book(239, 'jonas schmedtmann');
+lufthansa.book(635, 'john smith');
+console.log(lufthansa);
 
-greet('Hello')('Omid');
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
 
-const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+const book = lufthansa.book;
 
-greetArr('Hi')('Iman');
+// this DOES NOT WORK
+// because we don't have this keyword
+// the function is called as a regular function
+// book(23, 'Sarah Willians);
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'marry cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 593, 'omid farrokhi');
+console.log(swiss);
+
+// Apply method
+const flightData = [538, 'Goarge cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// but this is older
+// we don't use it that much now that we have spread
+book.call(swiss, ...flightData);
