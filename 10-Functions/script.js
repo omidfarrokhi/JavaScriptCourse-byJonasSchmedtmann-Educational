486ab3fae1bbeed1a4b7a14b8675b9e747320072
Omid <omidfarrokhii1381@gmail.com>
2026-09-24 21:56:34 +0330

    more closure examples

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,18 +1,49 @@
 'use strict';
 
-const secureBooking = function () {
-  let passengerCount = 0;
+// example 1
+// we don't necessirly need returning a function to have closure
+let f;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
   };
 };
 
-const booker = secureBooking();
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
 
-booker();
-booker();
-booker();
+g();
+f();
+console.dir(f);
 
-console.dir(booker);
+// re-assigning f function
+h();
+f();
+// it lost the previous closure and now has b in its closure
+console.dir(f);
+
+// example 2
+const boardPassenger = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start baording in ${wait} seconds`);
+};
+
+// the closure has priority to the scope chain
+// the function will use inner perGroup not the global one!!!!!!!!!!!!!!!!
+const perGroup = 1000;
+
+boardPassenger(180, 3);
+// we finish the function way before
+// but after the timer ends the callback still has access to function variables (the function that has finished execution way before)
