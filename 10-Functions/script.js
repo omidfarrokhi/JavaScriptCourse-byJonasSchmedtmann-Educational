ad49b0faff74c3ebab28236d8861b9e44d0175b8
Omid <omidfarrokhii1381@gmail.com>
2026-09-24 16:37:53 +0330

    the bind method

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -52,3 +52,59 @@ console.log(swiss);
 // but this is older
 // we don't use it that much now that we have spread
 book.call(swiss, ...flightData);
+
+// Bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'steven williams');
+
+// we can go even farther
+// assigning a preset argument value
+// Partial Application
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Jonas Schmedtmann');
+bookEW23('Martha Cooper');
+
+// another use-case with Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {};
+lufthansa.buyPlane = function () {
+  // the this actually is the button the event is attached to!!!!!!! not the lufthansa object
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+// the this actually is the button the event is attached to!!!!!!! not the lufthansa object
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
+
+// we use bind method
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+// a useful tax value
+// we don't care about the this keyword
+// because it is n
+const addVAT = addTax.bind(null, 0.23);
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+// writing it with higher-level functions
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
