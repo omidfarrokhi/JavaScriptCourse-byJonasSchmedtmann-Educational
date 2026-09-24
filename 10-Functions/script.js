d13cd5de77be03a11c35f1d7d3a05e4f851892a7
Omid <omidfarrokhii1381@gmail.com>
2026-09-24 20:13:30 +0330

    immediately invoked function expressions (IIFE)

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,40 +1,16 @@
 'use strict';
 
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Pythob', '2: Rust', '3: C++'],
-  // this generates [0, 0, 0, 0]
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    // get answer
-    const answer = Number(
-      prompt(`
-      ${this.question}\n
-      ${this.options.join('\n')}\n(Write option number)`),
-    );
+// we want a function that run one and only once
 
-    console.log(answer);
-
-    // register answer
-    typeof answer === 'number' &&
-      answer <= this.answers.length &&
-      this.answers[answer]++;
-
-    this.displayResults();
-    this.displayResults('string');
-  },
-
-  displayResults(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type === 'string') {
-      console.log(`Poll results are ${this.answers.join(', ')}`);
-    }
-  },
+// this can work but nothing is stopping us from calling the function again
+const runOnce = function () {
+  console.log('This will never run again');
 };
+runOnce();
 
-const answerPollBtn = document.querySelector('.poll');
-answerPollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll));
+// imidiently invoked function expression (IIFE)
+(function () {
+  console.log('This will never run again');
+})();
 
-poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
-poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+(() => console.log('This will also never run again'))();
