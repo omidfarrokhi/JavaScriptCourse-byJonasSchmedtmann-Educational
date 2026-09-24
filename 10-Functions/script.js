d457fada6cc248c1a1441b2ff0aa4b758f5b3abb
Omid <omidfarrokhii1381@gmail.com>
2026-09-24 20:06:14 +0330

    challenge #1

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,110 +1,40 @@
 'use strict';
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`,
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Pythob', '2: Rust', '3: C++'],
+  // this generates [0, 0, 0, 0]
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // get answer
+    const answer = Number(
+      prompt(`
+      ${this.question}\n
+      ${this.options.join('\n')}\n(Write option number)`),
     );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+
+    console.log(answer);
+
+    // register answer
+    typeof answer === 'number' &&
+      answer <= this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
 
-lufthansa.book(239, 'jonas schmedtmann');
-lufthansa.book(635, 'john smith');
-console.log(lufthansa);
+const answerPollBtn = document.querySelector('.poll');
+answerPollBtn.addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-
-const book = lufthansa.book;
-
-// this DOES NOT WORK
-// because we don't have this keyword
-// the function is called as a regular function
-// book(23, 'Sarah Willians);
-
-book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
-
-book.call(lufthansa, 239, 'marry cooper');
-console.log(lufthansa);
-
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
-
-book.call(swiss, 593, 'omid farrokhi');
-console.log(swiss);
-
-// Apply method
-const flightData = [538, 'Goarge cooper'];
-book.apply(swiss, flightData);
-console.log(swiss);
-
-// but this is older
-// we don't use it that much now that we have spread
-book.call(swiss, ...flightData);
-
-// Bind method
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-
-bookEW(23, 'steven williams');
-
-// we can go even farther
-// assigning a preset argument value
-// Partial Application
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('Jonas Schmedtmann');
-bookEW23('Martha Cooper');
-
-// another use-case with Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {};
-lufthansa.buyPlane = function () {
-  // the this actually is the button the event is attached to!!!!!!! not the lufthansa object
-  console.log(this);
-
-  this.planes++;
-  console.log(this.planes);
-};
-
-// the this actually is the button the event is attached to!!!!!!! not the lufthansa object
-// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane);
-
-// we use bind method
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
-
-// Partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-// a useful tax value
-// we don't care about the this keyword
-// because it is n
-const addVAT = addTax.bind(null, 0.23);
-
-console.log(addVAT(100));
-console.log(addVAT(23));
-
-// writing it with higher-level functions
-const addTaxRate = function (rate) {
-  return function (value) {
-    return value + value * rate;
-  };
-};
-
-const addVAT2 = addTaxRate(0.23);
-console.log(addVAT2(100));
-console.log(addVAT2(23));
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
