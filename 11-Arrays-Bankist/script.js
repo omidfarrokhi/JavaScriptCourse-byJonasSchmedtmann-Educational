a0b0e7e7aff993625a8ac093d520f1b36b9042ae
Omid <omidfarrokhii1381@gmail.com>
2026-09-26 00:24:49 +0330

    the map method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -92,25 +92,35 @@
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // ////////////////////////////////////////////////
-const checkDogs = function (dogsJulia, dogsKate) {
-  const dogsJuliaCorrected = [...dogsJulia];
-  dogsJuliaCorrected.splice(0, 1);
-  dogsJuliaCorrected.splice(-2);
+// THE MAP METHOD
 
-  const dogs = dogsJuliaCorrected.concat(dogsKate);
-  console.log(dogs);
+const eurToUsd = 1.1;
 
-  dogs.forEach(function (dog, i) {
-    if (dog >= 3) {
-      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
-    } else {
-      console.log(`Dog number ${9 + 1} is still a puppy`);
-    }
-  });
-};
+const movementsUsd = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// arrow
+const movementsUsdArrow = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUsd);
+console.log(movementsUsdArrow);
+
+// with for-of
+
+const movementsUsdFor = [];
+for (const mov of movements) {
+  movementsUsdFor.push(mov * eurToUsd);
+}
+console.log(movementsUsdFor);
+
+const movementsDescriptions = movements.map(
+  (mov, i, arr) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`,
+);
+
+console.log(movementsDescriptions);
