aa3f02571b7e8c938c4d19d96ff74cc1b5ef5997
Omid <omidfarrokhii1381@gmail.com>
2026-09-26 14:47:29 +0330

    the filter method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -130,3 +130,23 @@ const movementsDescriptions = movements.map(
 );
 
 console.log(movementsDescriptions);
+
+// FILTER METHOD
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+/// with for loop
+// const depositsFor = [];
+// for (const mov of movements) {
+//   if (mov > 0) {
+//     depositsFor.push(mov);
+//   }
+// }
+// console.log(depositsFor);
+
+// the arrow easier form
+const withdrawals = movements.filter(mov => mov < 0);
